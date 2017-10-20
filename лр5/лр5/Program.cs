0b5bd83e67db03c6500e210176b9b5bac4053909
Eduard110599 <eduard.ch8@gmail.com>
2017-10-20 13:08:00 +0300

    5лр

---
+++ лр5/лр5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лр5
+{
+    public abstract class Tovar
+    {
+        public abstract string name();
+        public abstract int kolichestvo();
+        public abstract double cena();
+    }
+    interface ITovar
+    {
+
+    }
+    public class Product:Tovar
+    {
+       
+        public string tyjpe()
+        {
+            return "type of product";
+        }
+
+    }
+    
+    
+    public abstract class KonditerIzdelie
+    {
+     
+    }
+    public  class Konfet: KonditerIzdelie
+    {
+        public  double kkal()
+        {
+            return 0;
+        }
+    }
+    public  class Tort: KonditerIzdelie
+    {
+        public  double kkal()
+        {
+            return 0;
+        }
+    }
+    public  class Cveti: Tovar
+    {
+
+    }
+    public  class Watch : Tovar
+    {
+
+    }
+    
+    class Program
+    {public abstract class Pechenie
+    {
+
+    }
+        static void Main(string[] args)
+        {
+       
+        }
+    }
+}
